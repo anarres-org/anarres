f12485b1c282f51dd9edaa6a2cb7e537b2315fc8
m0wer <m0wer@autistici.org>
2019-08-17 16:49:07 +0200

    [test] Added tests for Grafana.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -26,7 +26,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ("phpldapadmin"),
     ("bind_dns_server"),
     ("murmur"),
-    ("influxdb")
+    ("influxdb"),
+    ("grafana")
 ])
 def test_services_are_enabled_and_running(host, service):
     service = host.service("docker." + service + ".service")
