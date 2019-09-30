4d41127f1d9200c45c3096c24c6e74766d3fa0d9
m0wer <m0wer@autistici.org>
2019-09-30 22:29:15 +0200

    [test] Added testing configuration for Portainer.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -33,7 +33,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ("grafana"),
     ("home_assistant"),
     ("rstudio"),
-    ("jellyfin")
+    ("jellyfin"),
+    ("portainer")
 ])
 def test_services_are_enabled_and_running(host, service):
     service = host.service("docker." + service + ".service")
