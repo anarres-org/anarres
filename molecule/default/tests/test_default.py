6ecfbba12af98c15e54e9f5ce8894368efee22fe
m0wer <m0wer@autistici.org>
2019-11-26 13:03:12 +0100

    [test] Added molecule configurations for Moodle.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -35,7 +35,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ("rstudio"),
     ("jellyfin"),
     ("portainer"),
-    ("anki")
+    ("anki"),
+    ("moodle")
 ])
 def test_services_are_enabled_and_running(host, service):
     service = host.service("docker." + service + ".service")
